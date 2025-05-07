d150f34fe4341aab1260d835e7ef59d1b7f98b1f
kent <kent.ganteng2@gmail.com>
2025-05-07 19:34:45 +0800

    fix export

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -2,4 +2,6 @@ export * from './button';
 export * from './card';
 export * from './neobrutalism-provider';
 export * from './neobrutalism-style';
-export * from './example'; 
+export * from './motion-button';
+export * from './motion-card';
+export * from './buddy-card';
