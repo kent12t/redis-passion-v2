5e4ccbd67344b47325166c64e757f13eac8d82d4
kent <kent.ganteng2@gmail.com>
2025-07-23 18:00:28 +0800

    fix: missing file

---
+++ app/lib/text-utils.ts
@@ -0,0 +1,10 @@
+import textContent from '@/app/data/text_content_en.json';
+
+/**
+ * Get text content from the JSON file
+ */
+export function getTextContent() {
+  return textContent;
+}
+
+export default textContent; 
